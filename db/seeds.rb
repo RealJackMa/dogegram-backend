86442101007f6e447b36d0c22433b5c77df53dd4
CelestialEmperor <mapeiyao@gmail.com>
2020-07-29 19:38:06 +0100

    Fixed seeding bug.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Comment.destroy_all
 i1 = Image.create(title: "1GB Hello World", likes: 30, url: "https://i.kym-cdn.com/photos/images/facebook/001/853/912/df0.jpg")
 i2 = Image.create(title: "Pro Programmer", likes: 42, url: "https://3.bp.blogspot.com/-FAUTU4o90L4/UsbJ31plhGI/AAAAAAAAACg/N2GM5AfI2P0/s1600/doge+wow.png")
 i3 = Image.create(title: "Git/doge/master", likes: 12, url: "https://camo.githubusercontent.com/aec4c3fc2d674f2a4e90ed9e1413d95f204d7445/687474703a2f2f692e696d6775722e636f6d2f48784839716b612e706e67")
-i4 = Comment.create(content: "Jack Ma's Profile Pic", likes: 99999, url: "https://ih1.redbubble.net/image.859144446.2420/st,small,507x507-pad,600x600,f8f8f8.u2.jpg")
+i4 = Image.create(title: "Jack Ma's Profile Pic", likes: 999, url: "https://ih1.redbubble.net/image.859144446.2420/st,small,507x507-pad,600x600,f8f8f8.u2.jpg")
 
 c1 = Comment.create(content: "Such wow, very doge.", image: i1)
 c2 = Comment.create(content: "Love the meme, hate the background!", image: i1)
